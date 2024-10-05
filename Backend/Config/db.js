43ac0811212c578c8099ada5138e11fff3d8fe86
Vansh Dobariya <167301367+VanshBD@users.noreply.github.com>
2024-10-05 16:47:18 +0530

    fix

--- Backend/Config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const connect = async () => {
-  await mongoose.connect(process.env.MONGO_URL);
-  console.log("DB Connected Successfully");
-};
-
-module.exports = connect;
